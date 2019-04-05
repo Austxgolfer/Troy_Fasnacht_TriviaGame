3f5d51a5aa59d550803031a6bb24512a6ac5effc
MAX <47061140+Austxgolfer@users.noreply.github.com>
2019-04-05 16:24:40 -0500

    started from scratch with arrays.  added functions for answer check and added button clicks.  it is working at this time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,74 +1,122 @@
-var q1 = {quest:"The beaver is the national emblem of which country",fake4:"Canada",fake1:"Alaska",fake2:"Norway",fake3:"Russia",bimage:,};
-var q2 = {quest:"In which movie did Humphrey Bogart play Charlie Allnut?",fake4:"African Queen",fake1:"Casablanca",fake2:"The Caine Mutiny",fake3:"The Maltese Falcon",bimage:,};
-var q3 = {quest:"Which singer's real name is Stefani Joanne Angelina Germanotta?",fake4:"Lady Gaga",fake1:"Gwenn Stefani",fake2:"Cher",fake3:"Cardi B",bimage:,};
-var q4 = {quest:"How many players are there in a baseball team?",fake4:"9",fake1:"11",fake2:"25",fake3:"8",bimage:,};
-var q5 = {quest:"Which TV Character Said,'Live long and prosper'?",fake4:"Mr. Spock",fake1:"Mr. Rogers",fake2:"Leslie Knope",fake3:"Bart Simpson",bimage:,};
-var q6 = {quest:"What is the name of Batman's butler?",fake4:"Alfred",fake1:"George",fake2:"Belvedere",fake3:"Jimmy",bimage:,};
-var q7 = {quest:"The average human body contains how many pints of blood?",fake4:"9",fake1:"7",fake2:"10",fake3:"22",bimage:,};
-var q8 = {quest:"Which US state is nearest to the old Soviet Union?",fake4:"Alaska",fake1:"North Dakota",fake2:"California",fake3:"Washington",bimage:,};
-var q9 = {quest:"In which city is Jim Morrison buried?",fake4:"Paris",fake1:"New York",fake2:"Dallas",fake3:"Anehiem",bimage:,};
-var q10 = {quest:"`Hg' is the chemical symbol of which element?",fake4:"Mercury",fake1:"Iron",fake2:"Lead",fake3:"Radium",bimage:,};
-var q11 = {quest:"The Pyrenees mountain range separates which two European countries?",fake4:"France and Spain",fake1:"Canada and America",fake2:"Panama and Nicaragua",fake3:"Finland and Sweden",bimage:,};
-var q12 = {quest:"According to Greek mythology who was the first woman on earth?",fake4:"Pandora",fake1:"Eve",fake2:"Helen",fake3:"Gwenn",bimage:,};
-var q13 = {quest:"What names were given to the three tunnels in the movie `The Great Escape`?",fake4:"Tom, Dick and Harry",fake1:"Larry, Mo and Curly",fake2:"The Three Amigos",fake3:"Snap, Crackle and Pop",bimage:,};
-var q14 = {quest:"Which US state's largest city is Baltimore?",fake4:"Maryland",fake1:"Conneticut",fake2:"Virginia",fake3:"New Jersey",bimage:,};
-var q15 = {quest:"How is the groundnut better known?",fake4:"Peanut",fake1:"Acorn",fake2:"Pecan",fake3:"Walnut",bimage:,};
-var q16 = {quest:"Which country was formerly known as Abyssinia?",fake4:"Ethiopia",fake1:"Lybia",fake2:"Egypt",fake3:"Saudi Arabia",bimage:,};
-var q17 = {quest:"How many letters are there in the German alphabet?",fake4:"30",fake1:"26",fake2:"22",fake3:"35",bimage:,};
-var q18= {quest:"What is the most commonly diagnosed cancer in men?",fake4:"Prostate",fake1:"Skin Cancer",fake2:"Lung",fake3:"Brain",bimage:,};
-var q19 = {quest:"Who was the US president during World War I?",fake4:"Woodrow Wilson",fake1:"Teddy Roosevelt",fake2:"John Kennedy",fake3:"John McCarthy",bimage:,};
-var q20= {quest:"In which 1979 movie was the spaceship called Nostromo?",fake4:"Alien",fake1:"Star Trek",fake2:"The Black Hole",fake3:"Flash Gordon",bimage:,};
+var questions = [
+    "The beaver is the national emblem of which country",
+    "In which movie did Humphrey Bogart play Charlie Allnut?",
+    "Which singer's real name is Stefani Joanne Angelina Germanotta?",
+    "How many players are there in a baseball team?",
+    "Which TV Character Said,'Live long and prosper'?",
+    "What is the name of Batman's butler?",
+    "The average human body contains how many pints of blood?",
+    "Which US state is nearest to the old Soviet Union?",
+    "In which city is Jim Morrison buried?",
+    "`Hg' is the chemical symbol of which element?",
+    "The Pyrenees mountain range separates which two European countries?",
+    "According to Greek mythology who was the first woman on earth?",
+    "What names were given to the three tunnels in the movie `The Great Escape`?",
+    "Which US state's largest city is Baltimore?",
+    "How is the groundnut better known?",
+    "Which country was formerly known as Abyssinia?",
+    "How many letters are there in the German alphabet?",
+    "What is the most commonly diagnosed cancer in men?",
+    "Who was the US president during World War I?",
+    "In which 1979 movie was the spaceship called Nostromo?"
+];
+var answers = [["Canada","Alaska","Norway","Russia"],
+    ["African Queen","Casablanca","The Caine Mutiny","The Maltese Falcon"],
+    ["Lady Gaga","Gwenn Stefani","Cher","Cardi B"],
+    ["9","11","25","8"],
+    ["Mr. Spock","Mr. Rogers","Leslie Knope","Bart Simpson"],
+    ["Alfred","George","Belvedere","Jimmy"],
+    ["9","7","10","22"],
+    ["Alaska","North Dakota","California","Washington"],
+    ["Paris","New York","Dallas","Anehiem"],
+    ["Mercury","Iron","Lead","Radium"],
+    ["France and Spain","Colorado and Wyoming","Panama and Nicaragua","Finland and Sweden"],
+    ["Pandora","Eve","Helen","Gwenn"],
+    ["Tom, Dick and Harry","Larry, Mo and Curly","The Three Amigos","Snap, Crackle and Pop"],
+    ["Maryland","Conneticut","Virginia","New Jersey"],
+    ["Peanut","Acorn","Pecan","Walnut"],
+    ["Ethiopia","Lybia","Egypt","Saudi Arabia"],
+    ["30","26","22","35"],
+    ["Prostate","Skin Cancer","Lung","Brain"],
+    ["Woodrow Wilson","Teddy Roosevelt","John Kennedy","John McCarthy"],
+    ["Alien","Star Trek","The Black Hole","Flash Gordon"]
+];
+
 //Variables for the program
 var rndQuest = []; //array for random questions
 var rndAns = []; //Array for randomized answers
+var counter = 0;
+var corrCount =0;
+var Ans1;Ans2;Ans3;Ans4;
 
 function rndmSelect() { //selects 10 random questions and verifies no duplicates
     for (i = 0; i < 10; i++) {
-        var newVal = Math.floor((Math.random()*20)+1);
+        var newVal = Math.floor((Math.random()*19)+1);
         if (rndQuest.includes(newVal)){
         i = i - 1;
-        } else {rndQuest[i] = newVal;}
-    };
-
-    for (x = 0; x < 3; i++) {  //randomizes answer order
-        var newVal2 = Math.floor((Math.random()*3)+1);
+        } else {rndQuest[i] = newVal;
+        console.log(rndQuest);}
+        };
+    };// end of rndmSelect function
+function ansOrder(){
+    for (x = 0; x < 4; x++) {  //randomizes answer order
+        var newVal2 = Math.floor((Math.random()*4));
         if (rndAns.includes(newVal2)){
-        i = i - 1;
-        } else {rndAns[i] = newVal2;}    
+        x = x - 1;
+        } else {rndAns[x] = newVal2;
+        console.log(rndAns);}    
     };
-}; // end of rndmSelect function
-
+}; 
 function postQuest() {
-    for (i = 0; i < 10; i++){
-        var question = rndQuest[i]
-        var Ans1 = fake + rndAns[0];
-        var Ans2 = fake + rndAns[1];
-        var Ans3 = fake + rndAns[2];
-        var Ans4 = fake + rndAns[3];
-        document.getElementById(`displayQ`).innerHTML = question.quest;
-        document.getElementById(`Ans1`).innerHTML = question.Ans1;
-        document.getElementById(`Ans2`).innerHTML = question.Ans2;
-        document.getElementById(`Ans3`).innerHTML = question.Ans3;
-        document.getElementById(`Ans4`).innerHTML = question.Ans4;
-       
-       // document.getElementById(`number`).innerHTML = "Make another selection";
-    }// else { if (runningTot > targetNum) {
-      //  document.getElementById(`number`).innerHTML = "You lose!  You exceeded the target number.";
-      //  setTimeout(function(){gameReset();},5000);
-    }// else {document.getElementById(`number`).innerHTML = "You Win!  Game will Restart Automatically";
-       // setTimeout(function(){gameReset();},5000);
-};   
-};
+    if (counter > rndQuest.length-1) {
+        $("#spacer").text("Gameover. You got " + corrCount + "out of 10 questions correct."); 
+    } else {    
+        var question = questions[rndQuest[counter]];
+        Ans1 = answers[rndQuest[counter]][rndAns[0]];
+        Ans2 = answers[rndQuest[counter]][rndAns[1]];
+        Ans3 = answers[rndQuest[counter]][rndAns[2]];
+        Ans4 = answers[rndQuest[counter]][rndAns[3]];
+        $("#displayQ").text(question);
+        $("#Ans1").text(Ans1);
+        $("#Ans2").text(Ans2);
+        $("#Ans3").text(Ans3);
+        $("#Ans4").text(Ans4); 
+        };
 };
 
+function checkAnswer(boxchk) {
+    var correctAns = answers[rndQuest[counter]][0];
+    console.log(boxchk);
+    if (correctAns === boxchk) {
+        $("#spacer").text("That is correct!");
+        //var delay = setInterval($("#spacer").text(" "),1000);
+        //clearInterval(delay);
+        counter=counter + 1;
+        corrCount = corrCount + 1;
+        console.log(counter);
+        rndAns = [];
+        ansOrder();
+        postQuest();
+    } else { $("#spacer").text("Sorry that is incorrect!");
+    //var delay = setInterval($("#spacer").text(" "),1000);
+    //clearInterval(delay);
+        counter=counter + 1;
+        console.log(counter);
+        rndAns = [];
+        ansOrder();
+        postQuest();
+    };
+};
 
 
-//button clicks for the answers
 
-$(`#jewel0`).on(`click`,function() {totalUpdate(jewelVal[0])});
-$(`#jewel1`).on(`click`,function() {totalUpdate(jewelVal[1])});
-$(`#jewel2`).on(`click`,function() {totalUpdate(jewelVal[2])});
-$(`#jewel3`).on(`click`,function() {totalUpdate(jewelVal[3])});
+   
 
- //setTimeout(function(){document.getElementById(`displayVal`).innerHTML = " "},1000);
-        //setTimeout(function(){document.getElementById(`totalVal`).innerHTML = " "},1000);
+
+rndmSelect();
+ansOrder();
+postQuest();
+$("#Ans1").on("click",function() {checkAnswer(Ans1)});
+$("#Ans2").on("click",function() {checkAnswer(Ans2)});
+$("#Ans3").on("click",function() {checkAnswer(Ans3)});
+$("#Ans4").on("click",function() {checkAnswer(Ans4)});
